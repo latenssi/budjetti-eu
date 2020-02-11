4e4fda16a2f1318d0f49acfbf0d2116e0e7a33f0
Lauri <latenssi@gmail.com>
2020-02-11 22:29:51 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+export default function Index() {
+  return (
+    <main className="center">
+      <div className="text">Coming soon...</div>
+      <style jsx>{`
+        main {
+          width: 90%;
+          max-width: 900px;
+          margin: 300px auto;
+          text-align: center;
+        }
+        .text {
+          font-family: cursive;
+          color: #e243de;
+          font-size: 24px;
+          padding-bottom: 10px;
+        }
+      `}</style>
+    </main>
+  );
+}
